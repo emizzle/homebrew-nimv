e8689780fb85f930a46e0fb29d46aab122c2ae18
Eric <5089238+emizzle@users.noreply.github.com>
2025-02-10 19:29:26 +1100

    Update shasum on v0.0.1 (due to upstream changes)

--- Formula/nimv.rb
+++ Formula/nimv.rb
@@ -2,7 +2,7 @@ class Nimv < Formula
   desc "Simple nim version manager"
   homepage "https://github.com/emizzle/nimv"
   url "https://github.com/emizzle/nimv/archive/v0.0.1.tar.gz"
-  sha256 "79d0875adfd88e8986bf89da4ef6016684e6288e08ddd173182f98541f20e0af"
+  sha256 "d61ba2999be565be19d4145977718cadfdcc0eb9c677e79230127c528b09d7f7"
   version "0.0.1"
   
   depends_on "git"
