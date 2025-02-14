9e1b15015db87a1e514b939cc7c7e63284d137a3
Eric <5089238+emizzle@users.noreply.github.com>
2025-02-14 20:10:18 +1100

    v0.0.3

--- Formula/nimv.rb
+++ Formula/nimv.rb
@@ -1,9 +1,9 @@
 class Nimv < Formula
   desc "Simple nim version manager"
   homepage "https://github.com/emizzle/nimv"
-  url "https://github.com/emizzle/nimv/archive/0.0.2.tar.gz"
-  sha256 "f86972b4012fe3dd6734f0c90d63cd630a7605079e70e8009794ff0ea6d56022"
-  version "0.0.2"
+  url "https://github.com/emizzle/nimv/archive/0.0.3.tar.gz"
+  sha256 "23434a602aae98441cf1f36c0dc6b0635cbeeaa16ae9c33f3c5b7e05cadfe87c"
+  version "0.0.3"
   
   depends_on "git"
   depends_on "curl"
